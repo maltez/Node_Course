78e0ee2c09744660e7288040b0fb029c0789c657
Alexey Shkumat <Alexey.Shkumat@hotmail.com>
2018-03-31 08:04:03 +0300

    home-work-2, first part

---
+++ home-work/shkumat_alexey/hw2_genFactorial.js
@@ -0,0 +1,15 @@
+// Получение факториалов с помощью функции-генератора
+// https://ru.wikipedia.org/wiki/Факториал
+
+var	factorial =	function * (n) {
+	if	( n < 1 )
+		return; 
+	let	current	=	1 ; 
+	for( let i=0 ; i < n ; i+=1 ) { 
+		yield	current; 
+		current  = current * ( i + 1 ); 
+	} 
+}
+
+for( let item of factorial(7) )
+	console.log( item );	//  1  1  2  6  24  120  720
