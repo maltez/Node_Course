81d6a088afb9f5ad182d31afc44df0f1376b5100
Alexey Shkumat <Alexey.Shkumat@hotmail.com>
2018-04-01 09:19:57 +0300

    home-work-2, 3rd part

---
+++ home-work/shkumat_alexey/hw2_promises.js
@@ -0,0 +1,35 @@
+// Выполнение цепочки из функций (две параллельных, затем две последовательных)
+// с накопление результатов выполн. каждого шага. В конце выводятся результаты
+// выполнения двух последних шагов. Локальные переменные не используются.
+
+const	step1Parallel = function( resolve, reject ) {
+	setTimeout( ()=> {
+			return	resolve( "1-st step returns A" );		
+		}
+	,	100 
+	);	
+}
+,	step2Parallel = function( resolve, reject ) {
+	setTimeout( ()=> {
+			return	resolve( "2-nd step returns B" );		
+		}
+	,	500 
+	);	
+}
+,	step3Serial = function( data ) {
+	return	[ ...data , "3-rd step returns C" ];
+}
+,	step4Serial = function( data ) {
+	return	[ ...data , "4-th step returns D" ];
+}
+
+Promise.all([
+	new Promise( step1Parallel )
+, 	new Promise( step2Parallel )
+]).then( (data) => {
+	return	new Promise( ( resolve, reject ) => {
+			resolve( step4Serial ( step3Serial ( data ) ) ); 
+		});
+}).then( (data) => {
+	console.log( data.splice( data.length - 2 , 2 ) )
+})
