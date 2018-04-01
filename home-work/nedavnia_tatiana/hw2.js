5999ef7dfdb1db09e5ccac741dd66a32133dc524
Tania <nedavniat@gmail.com>
2018-04-01 09:05:04 +0300

    promise-fix

--- home-work/nedavnia_tatiana/hw2.js
+++ home-work/nedavnia_tatiana/hw2.js
@@ -63,6 +63,14 @@ const getLength = str => str.length;
 
 Promise.all([ getPromisePlaceholder('hello', 1000), getPromisePlaceholder('kitty', 2000) ])
   .then(([ str1, str2 ]) => `${str1} ${str2}`)
+  .then(capitalize)
+  .then(getLength)
+  .catch(err => console.error(err.message));
+
+//or this way in case functions to be fulfilled are synchronous (as in my case)
+Promise.all([ getPromisePlaceholder('hello', 1000), getPromisePlaceholder('kitty', 2000) ])
+  .then(([ str1, str2 ]) => `${str1} ${str2}`)
   .then(val => Promise.all([capitalize(val), getLength(val)]))
   .then(([ value, length ]) => ({ value, length }))
   .catch(err => console.error(err.message));
+
