c68274b272ad856185150de572944dba942efcda
Oleh Pashchenko <olehpashchenko@Olehs-MacBook-Pro.local>
2018-04-03 21:47:56 +0300

    Updated singletone

--- home-work/oleh_pashchenko/03/singletone.js
+++ home-work/oleh_pashchenko/03/singletone.js
@@ -14,3 +14,34 @@ const singletone = (function() {
 
 console.log(...singletone.getRandomNumbers(5));
 console.log(...singletone.getRandomNumbers(2));
+
+
+
+const singletoneClass = (function() {
+  let instance;
+  class Singletone {
+    constructor() {
+      this.getRandomNumbers =  function* (n) {
+        for (let index = 0; index < n; index += 1) {
+          yield Math.random();
+        }
+      }
+    }
+  }
+
+  const create = function createInstance() {
+    if(!instance) instance = new Singletone();
+
+    return instance;
+  }
+
+return {
+  create
+}
+
+})();
+
+const singletoneInstance = singletoneClass.create();
+
+console.log(...singletoneInstance.getRandomNumbers(5));
+console.log(...singletoneInstance.getRandomNumbers(2));
