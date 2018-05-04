39d3e24e644f5c9ac7879ae49eafc8612e141d9f
Alexey Shkumat <Alexey.Shkumat@hotmail.com>
2018-05-04 22:38:55 +0300

    home-work-8, modified

--- home-work/shkumat_alexey/hw8_mini-express/mini-express.js
+++ home-work/shkumat_alexey/hw8_mini-express/mini-express.js
@@ -1,38 +1,56 @@
-// Based on http server create your own 'express' with: Routing , Body parser, Send json
-const	{ createServer } = require('http');
+// Based on http server create your own 'express' with: Routing
+const	{createServer} = require('http');
 
 class	MiniExpress {
 	constructor() {
 		this.routes	=	[];
+		this.methods	=	[];
 		this.handlers	=	[];
 		this.server	=	createServer();
 		this.server.on( 'request', this.handler.bind( this ) );
 	}
+
 	static	miniExpress() {
 		return	new MiniExpress();
 	}
-	listen( port, fn ){
+
+	listen( port, fn ) {
 		this.server.listen( port ,'localhost', fn );
 	}
+
 	get( route, handler ) {
+		this.methods.push( 'get' );
 		this.handlers.push( handler );
 		this.routes.push( route.toString().toLowerCase().trim() );
 	}
-	use() {
+	
+	post( route, handler ) {
+		this.methods.push( 'post' );
+		this.handlers.push( handler );
+		this.routes.push( route.toString().toLowerCase().trim() );
 	}
+
+	put( route, handler ) {
+		this.methods.push( 'put' );
+		this.handlers.push( handler );
+		this.routes.push( route.toString().toLowerCase().trim() );
+	}
+	
 	handler( req, res ) {
-		let	handler	;
-		const	current	=	req.url.toLowerCase().trim();
-		for( let i=0 ; i < this.routes.length ; i+=1 ) {
+		let	handler	= null;		
+		const	method	=	req.method.toLowerCase().trim() ,	
+			route	=	req.url.toLowerCase().trim() ;
+						
+		for ( let i = 0 ; i < this.routes.length ; i += 1 ) {
 			if ( !handler ) {
-				if ( this.routes[i] == current ) {
+				if ( this.routes[i] === route && this.methods[i] === method ) {
 					handler	= this.handlers[i];
 				} 
 			}
 		} 
-		for( let i=0 ; i < this.routes.lengh ; i+=1 ) {
+		for ( let i = 0 ; i < this.routes.lengh ; i += 1 ) {
 			if ( !handler ) {
-				if ( current.startsWith( this.routes[i] ) ) {
+				if ( route.startsWith( this.routes[i]  && this.methods[i] === method ) ) {
 					handler	= this.handlers[i];
 				} 
 			}
@@ -47,14 +65,4 @@ class	MiniExpress {
 	}
 }
 
-const	bodyParser = {
-	text() {
-		return	function( req , res , next ) {
-			res.send( "Text perser is working..." );
-			next();
-		}
-	} 
-}
-
 module.exports.miniExpress	=	MiniExpress.miniExpress;
-module.exports.bodyParser	=	bodyParser;
